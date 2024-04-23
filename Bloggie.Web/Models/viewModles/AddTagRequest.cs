f3f42d432b3a74bb0bde7dbcecc8dd918fab1948
malimo326 <mahmoodali2965.ma@gmail.com>
2024-04-23 19:04:44 +0200

    Connecting to database

---
+++ Bloggie.Web/Models/viewModles/AddTagRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Bloggie.Web.Models.viewModles
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
