cd86ff4dd125f820e4c22adc414fb358163a961d
malimo326 <mahmoodali2965.ma@gmail.com>
2024-04-24 10:35:06 +0200

    Add/Edit & Delete Tags Done

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -31,7 +31,71 @@ namespace Bloggie.Web.Controllers
 
             bloggieDbContext.Tags.Add(tag);
             bloggieDbContext.SaveChanges();
-            return View("Add");
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            // use dbContext to read the tags
+            var tags = bloggieDbContext.Tags.ToList();
+
+            return View(tags);
+        }
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            // 1st method
+            // var tag = bloggieDbContext.Tags.Find(id);
+
+            // 2nd Method
+            var tag = bloggieDbContext.Tags.FirstOrDefault(t => t.Id == id);
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+
+                    DisplayName = tag.DisplayName
+                };
+                return View(editTagRequest);
+            }
+            return View(null);
+        }
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+            var existingTag = bloggieDbContext.Tags.Find(tag.Id);
+            if (existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+                // save
+                bloggieDbContext.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+        [HttpPost]
+        public IActionResult Delete(EditTagRequest editTagRequest)
+        {
+            var tag = bloggieDbContext.Tags.Find(editTagRequest.Id);
+            if (tag != null)
+            {
+                bloggieDbContext.Tags.Remove(tag);
+                bloggieDbContext.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return RedirectToAction("Edit", new {id = editTagRequest.Id});
         }
     }
 }
---
+++ Bloggie.Web/Models/viewModles/EditTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggie.Web.Models.viewModles
+{
+    public class EditTagRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
