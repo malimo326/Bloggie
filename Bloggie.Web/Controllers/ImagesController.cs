3a38f0750339d4ce5adef6b343f7c5f924e5e243
malimo326 <mahmoodali2965.ma@gmail.com>
2024-04-25 14:09:21 +0200

    Using Cloudinary and implement the upload method

---
+++ Bloggie.Web/Controllers/ImagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile File)
+        {
+            // call a repo
+
+        }
+    }
+}
