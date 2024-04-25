5bc4d0b38ffe11f662bc85b7ef58461837460e70
malimo326 <mahmoodali2965.ma@gmail.com>
2024-04-25 19:52:04 +0200

    Displaying Tags on Home Page

--- Bloggie.Web/Controllers/HomeController.cs
+++ Bloggie.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Bloggie.Web.Models;
+using Bloggie.Web.Models.viewModles;
 using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,17 +10,32 @@ namespace Bloggie.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly ITagRepository tagRepository;
 
-        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+        public HomeController(ILogger<HomeController> logger,
+            IBlogPostRepository blogPostRepository,
+            ITagRepository tagRepository)
         {
             _logger = logger;
             this.blogPostRepository = blogPostRepository;
+            this.tagRepository = tagRepository;
         }
 
         public async Task<IActionResult> Index()
         {
+            // GETTING ALL BLOG 
+            
           var blogPosts = await blogPostRepository.GetAllAsync();
-            return View(blogPosts);
+            // get all tags
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new HomeViewModel
+            {
+                blogPosts = blogPosts,
+                tags = tags
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
