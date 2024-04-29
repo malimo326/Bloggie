b3167d980403202403abac1114dcd2240a368239
malimo326 <mahmoodali2965.ma@gmail.com>
2024-04-29 15:14:22 +0200

    Register Page - Server Side Validations

--- Bloggie.Web/Controllers/AccountController.cs
+++ Bloggie.Web/Controllers/AccountController.cs
@@ -25,26 +25,30 @@ namespace Bloggie.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
-            var identityUser = new IdentityUser
+            if (ModelState.IsValid)
             {
-                UserName = registerViewModel.Username,
-                Email = registerViewModel.Email
-            };
-
-            var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
-
-            if (identityResult.Succeeded)
-            {
-                // assign this user the "User" role
-                var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
-                if (roleIdentityResult.Succeeded)
+                var identityUser = new IdentityUser
                 {
-                    // show success notification
-                    return RedirectToAction("Register");
+                    UserName = registerViewModel.Username,
+                    Email = registerViewModel.Email
+                };
+
+                var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
+
+                if (identityResult.Succeeded)
+                {
+                    // assign this user the "User" role
+                    var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
+                    if (roleIdentityResult.Succeeded)
+                    {
+                        // show success notification
+                        return RedirectToAction("Register");
 
 
+                    }
                 }
             }
+           
             // show error notification
 
             return View();
--- Bloggie.Web/Models/viewModles/RegisterViewModel.cs
+++ Bloggie.Web/Models/viewModles/RegisterViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace Bloggie.Web.Models.viewModles
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bloggie.Web.Models.viewModles
 {
     public class RegisterViewModel
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage ="Password has to be at least 6 characters")]
         public string Password { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
