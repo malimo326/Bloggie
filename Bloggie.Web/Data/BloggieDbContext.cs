7e50165c76b7bde6405e9d0d71e1c63c8f62d23e
malimo326 <mahmoodali2965.ma@gmail.com>
2024-04-29 12:32:50 +0200

    Domin Models For Comment Functionality & Migration

--- Bloggie.Web/Data/BloggieDbContext.cs
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -12,5 +12,7 @@ namespace Bloggie.Web.Data
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<BlogPostLike> blogPostLikes { get; set; }
+        public DbSet<BlogPostComment> blogPostComment { get; set; }
+
     }
 }
